7f15c95b3897dc8f5d78460e20246392c53d990c
fluffypancakes159 <cdiep@stuy.edu>
2018-01-05 14:38:14 -0500

    made Talkable a working interface

--- Talkable.java
+++ Talkable.java
@@ -1,33 +1,5 @@
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.Scanner;
-import java.util.List;
-import java.util.ArrayList;
-import java.util.Random;
 public interface Talkable {
 
-    public String speak(String context){
-        try {
-            //read the file into a list of Strings
-            Scanner sc = new Scanner(new File(context + ".txt"));
-            List<String> lines = new ArrayList<String>();
-
-            //adding each newline to an ArrayList
-            while (sc.hasNextLine()) {
-                lines.add(sc.nextLine());
-            }
-            //return a random element of the array
-            Random randgen = new Random();
-            return lines.get(randgen.nextInt(lines.size()-1));
-
-            //reminder the python is better than java:
-            //in python this code is:
-
-            //from random import choice
-            //speak = lambda context: choice([i for i in open(context+'.txt', 'r')])
-        } catch (FileNotFoundException f){
-            return "";
-        }
-
-    }
+    public String speak ( String context );
+    
 }
