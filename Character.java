c255d18deae6755599e3012f6720253affc58191
fluffypancakes159 <cdiep@stuy.edu>
2018-01-05 14:38:48 -0500

    implemented Talkable in character

--- Character.java
+++ Character.java
@@ -1,4 +1,11 @@
-public abstract class Character {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Random;
+
+public abstract class Character implements Talkable {
 
     public int maxhp;
     public int currenthp;
@@ -10,6 +17,29 @@ public abstract class Character {
     
     public abstract void attack ( Player other );
 
-    public abstract void speak ( String context );
+    public String speak(String context) {
+        try {
+            //read the file into a list of Strings
+            Scanner sc = new Scanner(new File(context + ".txt"));
+            List<String> lines = new ArrayList<String>();
+
+            //adding each newline to an ArrayList
+            while (sc.hasNextLine()) {
+                lines.add(sc.nextLine());
+            }
+            //return a random element of the array
+            Random randgen = new Random();
+            return lines.get(randgen.nextInt(lines.size()-1));
+
+            //reminder the python is better than java:
+            //in python this code is:
+
+            //from random import choice
+            //speak = lambda context: choice([i for i in open(context+'.txt', 'r')])
+        } catch (FileNotFoundException f){
+            return "";
+        }
+
+    }
     
 }
