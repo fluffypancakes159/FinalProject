f7ca0918f1687842ec82dcf8709b73c195c2cafc
fluffypancakes159 <cdiep@stuy.edu>
2018-01-08 14:44:08 -0500

    added attacking functions and comments

--- Character.java
+++ Character.java
@@ -16,8 +16,31 @@ public abstract class Character implements Talkable {
     public int gold;
     public String name;
     
-    public abstract boolean attack ( Player other );
+    public boolean attack ( Character other ) {// attack function
+	if ( (int)Math.random() * 100 < 90 ) { // checks if the attack hit
+	    damage ( other );                  // damages opponent then checks
+	    killCheck ( other );               // if they are killed
+	    return true;                       // returns true for successful hit
+	}
+	else {
+	    return false;                      // returns false for a miss
+	}
+    }
 
+    public boolean damage ( Character other ) { // helper function that changes hp
+	int damage = atk - def;                 // calculates total damage
+	if ( damage < 0 ) {                     // makes sure weak attacks 
+	    damage = 0;                         // heal the enemy instead
+	}
+	other.currenthp = other.currenthp - damage; // decreases other hp by damage
+	return true;
+    }
+
+    public static boolean killCheck ( Character other ) {
+	return currenthp <= 0;
+    }
+    
+    /*
     public String speak(String context) {
         try {
             //read the file into a list of Strings
@@ -41,6 +64,6 @@ public abstract class Character implements Talkable {
             return "";
         }
 
-    }
+    } */
     
 }
