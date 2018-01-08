38347b1ca24411acb62c56f12e0a70f40510045f
fluffypancakes159 <cdiep@stuy.edu>
2018-01-08 14:25:43 -0500

    name is characteristic of all characters

--- Character.java
+++ Character.java
@@ -14,7 +14,7 @@ public abstract class Character implements Talkable {
     public int spd;
     public int exp;
     public int gold;
-    public int name;
+    public String name;
     
     public abstract boolean attack ( Player other );
 
