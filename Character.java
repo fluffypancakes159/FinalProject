12dad32002f516b20c30e1b76cfc03b4ae468591
fluffypancakes159 <cdiep@stuy.edu>
2018-01-09 14:35:06 -0500

    made constructors easier to read and more modular

--- Character.java
+++ Character.java
@@ -17,19 +17,16 @@ public abstract class Character implements Talkable {
     public String name;
     
     public Character(){
-        maxhp = 30;
-        currenthp = maxhp;
-        atk = 5;
-        def = 2;
-        spd = 1;
-        exp = 0;
-        gold = 0;
-        name = "Test";
+        this ( 30 , 5 , 2 , 1 , 0 , 0 , "Test" );
+    }
+
+    public Character ( String n ) {
+        this ( 30 , 5 , 2 , 1 , 0 , 0 , n );
     }
 
     public Character ( int a , int b , int c , int d , int e , int f , String n ) {
         maxhp = a;
-        currenthp = maxhp;
+        currenthp = a;
         atk = b;
         def = c;
         spd = d;
@@ -39,27 +36,27 @@ public abstract class Character implements Talkable {
     }
 
     public boolean attack ( Character other ) {// attack function
-	if ( (int)Math.random() * 100 < 90 ) { // checks if the attack hit
-	    damage ( other );                  // damages opponent then checks
-	    killCheck ( other );               // if they are killed
-	    return true;                       // returns true for successful hit
-	}
-	else {
-	    return false;                      // returns false for a miss
-	}
+	    if ( (int)Math.random() * 100 < 90 ) { // checks if the attack hit
+	        damage ( other );                  // damages opponent then checks
+	        killCheck ( other );               // if they are killed
+	        return true;                       // returns true for successful hit
+	    }
+	    else {
+	        return false;                      // returns false for a miss
+	    }
     }
 
     public boolean damage ( Character other ) { // helper function that changes hp
-	int damage = atk - def;                 // calculates total damage
-	if ( damage < 0 ) {                     // makes sure weak attacks 
-	    damage = 0;                         // heal the enemy instead
-	}
-	other.currenthp = other.currenthp - damage; // decreases other hp by damage
-	return true;
+	    int damage = atk - def;                 // calculates total damage
+	    if ( damage < 0 ) {                     // makes sure weak attacks 
+	        damage = 0;                         // heal the enemy instead
+	    }
+	    other.currenthp = other.currenthp - damage; // decreases other hp by damage
+	    return true;
     }
 
     public static boolean killCheck ( Character other ) { // checks if character is dead
-	return currenthp <= 0;
+	    return currenthp <= 0;
     }
     
     /*
