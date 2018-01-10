a8c7e49c6c7293681bc499a5d984bb1557c0283e
fluffypancakes <cdiep@stuy.edu>
2018-01-09 20:44:15 -0500

    removed name field from Player, non-default contructor instantiates Player with level as well

--- Player.java
+++ Player.java
@@ -1,14 +1,14 @@
 public class Player extends Character {
 
     public int level;
-    public int name;
 
     public Player ( ) {
     	this ( "Player" );
     }
 
     public Player ( String n ) {
-    	this ( n );
+    	super ( n );
+    	level = 0;
     }
 
 }
