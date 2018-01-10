d4ae36c534cba720b970040684900d2500b9b66a
fluffypancakes <cdiep@stuy.edu>
2018-01-09 20:36:52 -0500

    added easier to read battle messages

--- Character.java
+++ Character.java
@@ -36,30 +36,37 @@ public abstract class Character /* implements Talkable */ {
     }
 
     public boolean attack ( Character other ) {// attack function
-	    if ( (int)Math.random() * 100 < 90 ) { // checks if the attack hit
-	        damage ( other );                  // damages opponent then checks
+	    if ( (int)(Math.random() * 100) < 85 ) { // checks if the attack hit
+	        // damage ( other );                 // damages opponent then checks
+            System.out.println( this.name + " attacks " + other.name + " for " + damage ( other ) + " damage" );
 	        other.aliveCheck ( );               // if they are killed
 	        return true;                       // returns true for successful hit
 	    }
 	    else {
-            System.out.println( "Miss!" );
+            System.out.println( this.name + " misses!" );
 	        return false;                      // returns false for a miss
 	    }
     }
 
-    public boolean damage ( Character other ) { // helper function that changes hp
+    public int damage ( Character other ) { // helper function that changes hp
 	    int damage = atk - def;                 // calculates total damage
 	    if ( damage < 0 ) {                     // makes sure weak attacks 
 	        damage = 0;                         // heal the enemy instead
 	    }
 	    other.currenthp = other.currenthp - damage; // decreases other hp by damage
-	    return true;
+	    return damage;
     }
 
     public boolean aliveCheck ( ) { // checks if character is dead
-	    return currenthp >= 0;
+	    return currenthp > 0;
     }
     
+    public static void battleUpdate ( Character player , Character other ) {
+        System.out.println ( "Player HP: " + player.currenthp + " / " + player.maxhp );
+        System.out.println ( "Enemy HP: " + other.currenthp + " / " + other.maxhp );
+        System.out.println ( "~~~~~~~~~~~~" ); 
+    }
+
     /*
     public String speak(String context) {
         try {
