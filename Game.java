ab7ac26d35ca71715094f3f88c1d2d9a42d864bf
fluffypancakes <cdiep@stuy.edu>
2018-01-09 20:40:26 -0500

    added currentLocation field and setter, moved battleUpdate here

---
+++ Game.java
@@ -0,0 +1,19 @@
+public class Game {
+
+	String currentLocation;
+
+	public void setCurrentLocation ( String l ) {
+		currentLocation = l;
+	}
+
+	public static void main(String[] args) {
+		
+	}
+
+	public static void battleUpdate ( Character player , Character other ) {
+        System.out.println ( "Player HP: " + player.currenthp + " / " + player.maxhp );
+        System.out.println ( "Enemy HP: " + other.currenthp + " / " + other.maxhp );
+        System.out.println ( "~~~~~~~~~~~~" ); 
+    }
+	
+}
