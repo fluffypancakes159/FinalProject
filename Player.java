dd2f879b36417c818b9bbf3d3a1f8cd0901479bc
fluffypancakes <cdiep@stuy.edu>
2018-01-09 20:26:30 -0500

    added constructors for Player

--- Player.java
+++ Player.java
@@ -3,4 +3,12 @@ public class Player extends Character {
     public int level;
     public int name;
 
+    public Player ( ) {
+    	this ( "Player" );
+    }
+
+    public Player ( String n ) {
+    	this ( n );
+    }
+
 }
