915b79e5c6b83869fe0fae0addd6f30ae23dd6f1
apeng <apeng1@stuy.edu>
2018-01-15 11:29:39 -0500

    added primitives inside action and also finished codefight mechanic

---
+++ codefight.py
@@ -0,0 +1,10 @@
+#Welcome to the codefight! Each time you close this window your code will be aut
+#omatically run with the test cases. If this window reopens, you have failed. Yo
+#u can fail from either incorrect output or failing to meet the time constraints
+#. If this window doesn't reopen, you passed the test! Of course, coding will be
+# in python. Writing this in java was completely awful so I decided to spare you
+# the pain and suffering of not having any primitives.
+
+
+#
+func = lambda x: x + 1
