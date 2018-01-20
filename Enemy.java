7dbecf8e8e09d93b1f4b116c1821e73f0fdb471d
fluffypancakes <cdiep@stuy.edu>
2018-01-20 17:48:45 -0500

    gave enemies levels so the level field as a result of the level field being for all Characters

--- Enemy.java
+++ Enemy.java
@@ -10,6 +10,7 @@ public class Enemy extends Character {
 
 	public Enemy ( int hp , int atk , int def , int spd , int exp , int gold , String name ) {
         super ( hp , atk , def , spd , exp , gold , name );
+        level = 1;
     }
 
 }
