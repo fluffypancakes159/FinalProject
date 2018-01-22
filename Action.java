a939692dd5d1c3d3b8805c4208b69eef54373d28
fluffypancakes <cdiep@stuy.edu>
2018-01-22 00:59:50 -0500

    fixed bugs bribe action

--- Action.java
+++ Action.java
@@ -156,15 +156,19 @@ public class Action{
             ((Player)args[1]).receive((Item)args[2]);
             Game.delayedPrintShort( "Received " + ((Item)args[2]).name + "!");
         }
-        if (args[0].equals("increment")) {
-            ((int)args[1]) += ((int)args[2]);
-        }
         if (args[0].equals("heal")) {
             ((Player)args[1]).heal((int)args[2]);
             Game.delayedPrintShort( "Healed " + args[2] + " HP!");
         }
         if (args[0].equals("set")) {
             args[1] = args[2];
+        }
+        if (args[0].equals("bribe")) {
+            if ( ((Player)args[1]).gold < 50 ) {
+                Game.delayedPrintLong( "Mr. K: You don't have enough money! The fact that you attempted this warrants (figurative) death!");
+                ((Player)args[1]).damage( ((Player)args[1]).maxhp );
+                ((Player)args[1]).die( new Enemy( ) );
+            }
         }
     }
 
